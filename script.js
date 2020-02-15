5a6ebaff8378d2ec2ad472436579f6071250b15d
Robert Harris <rlh9@rice.edu>
2020-02-15 15:40:10 -0600

    html and psueudocode for js created

---
+++ script.js
@@ -0,0 +1,116 @@
+// Global Variables
+var searchBtn = $(".searchBtn")
+var newLocation = $(".newLocation")
+// on clicking the search button 
+searchBtn.on("click", function(e){
+    // search the api for the info we need 
+
+
+
+    // then 
+
+    //create new row for card
+    // create a card 
+    // create a h4 tag 
+        // display the location and current date in the h4
+        // create img tag for the Symbol
+            //populate symbol with appropriate weather image based on data
+            // append image into h4
+    //create a p tag for temp
+        //fill text with temp data
+    //create a p tag for humidity
+        //fill text with humidity data
+    //create a p tag for wind speed
+        //fill text with wind speed data
+    //create a p tag for UV index  
+        //fill text with UV index data
+    //append h4 tag to card
+    //append temp tag to card
+    //append humid tag to card
+    //append wind tag to card
+    //append UV tag to card
+    //append card to new row
+
+    // create second row for 5 day forecast
+    
+    //create 5 day forecast h4
+        //h4 text is 5 Day Forecast
+    //append h4 to row 
+
+    // create div for day 1
+        // create day 1 card 
+            // populate card with content 
+                // p of that day's date
+                // img of the weather symbol
+                // p of temp
+                // p of humidity
+            // append date to card
+            // append img to card
+            // append temp to card
+            // append humidity to card
+        // append card to div
+        // append div to row 
+
+    // create div for day 2
+        // create day 2 card 
+            // populate card with content 
+                // p of that day's date
+                // img of the weather symbol
+                // p of temp
+                // p of humidity
+            // append date to card
+            // append img to card
+            // append temp to card
+            // append humidity to card
+        // append card to div
+        // append div to row
+
+    // create div for day 3
+        // create day 3 card 
+            // populate card with content 
+                // p of that day's date
+                // img of the weather symbol
+                // p of temp
+                // p of humidity
+            // append date to card
+            // append img to card
+            // append temp to card
+            // append humidity to card
+        // append card to div
+        // append div to row 
+
+    // create div for day 4
+        // create day 4 card 
+            // populate card with content 
+                // p of that day's date
+                // img of the weather symbol
+                // p of temp
+                // p of humidity
+            // append date to card
+            // append img to card
+            // append temp to card
+            // append humidity to card
+        // append card to div
+        // append div to row 
+
+    // create div for day 5
+        // create day 5 card 
+            // populate card with content 
+                // p of that day's date
+                // img of the weather symbol
+                // p of temp
+                // p of humidity
+            // append date to card
+            // append img to card
+            // append temp to card
+            // append humidity to card  
+        // append card to div
+        // append div to row 
+
+
+    
+    // append row 1 to newLocation div 
+    // append row 2 to newLocation div 
+
+
+});
