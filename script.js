18fb7e60e554b57474e567990697bcccbb468fd8
Robert Harris <rlh9@rice.edu>
2020-02-17 10:45:19 -0600

    javascript complete

--- script.js
+++ script.js
@@ -1,146 +1,229 @@
 // Global Variables
-var searchBtn = $(".searchBtn")
-var newLocation = $(".newLocation")
-
+var searchBtn = $(".searchBtn");
+var newLocation = $(".newLocation");
+var resultsArea = $(".resultsArea")
 // on clicking the search button 
 searchBtn.on("click", function(e){
-    e.preventDefault()
+    e.preventDefault();
     // create a new button 
-    var cityButton = $("<button>")
+    var cityButton = $("<button>");
     // button text becomes search query 
     cityButton.text($("#searchBar").val());
     // give button class of citybutton 
-    cityButton.addClass("citybutton btn btn-primary col align-self-end border border-white")
+    cityButton.addClass("citybutton btn btn-primary col align-self-end border border-white");
     //append citybutton to searchbar
     newLocation.append(cityButton);
 });
 
 
-$(".citybutton").on("click",function(e){
-    e.preventDefault();
-    var city = this.text
-    var queryurlCurrent = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=2d91d7d9be19dd3bb1c0b154fb2ca123"
-    var queryurl5Day = "api.openweathermap.org/data/2.5/forecast?q="+ city + "&appid=2d91d7d9be19dd3bb1c0b154fb2ca123"
-    
+$(".citybutton").on("click",function(event){
+    event.preventDefault();
+    var city = this.text;
+    var date = moment().format("MMM Do YYYY");
+    var queryurlCurrent = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=2d91d7d9be19dd3bb1c0b154fb2ca123";
+    var queryurl5Day = "api.openweathermap.org/data/2.5/forecast?q="+ city + "&appid=2d91d7d9be19dd3bb1c0b154fb2ca123";
+
     // search the api for the info we need for current day 
     $.ajax({
         url: queryurlCurrent,
         method: "GET"
-    }).then(function(e){
+    }).then(function(response){
+        console.log(response);
     // then 
-    console.log(response)
-    //create new row for card
-    // create a card 
-    // create a h4 tag 
-        // display the location and current date in the h4
-        // create img tag for the Symbol
-            //populate symbol with appropriate weather image based on data
-            // append image into h4
-    //create a p tag for temp
-        //fill text with temp data
-    //create a p tag for humidity
-        //fill text with humidity data
-    //create a p tag for wind speed
-        //fill text with wind speed data
-    //create a p tag for UV index  
-        //fill text with UV index data
-    //append h4 tag to card
-    //append temp tag to card
-    //append humid tag to card
-    //append wind tag to card
-    //append UV tag to card
-    //append card to new row
 
-})
+    //create new row for card
+    var newRow = $("<div>").addClass("row");
+    // create a card 
+    var newCard = $("<div>").addClass("card");
+    // create a h4 tag 
+    var title = $("<h4>");
+        // display the location and current date in the h4
+        title.text(city + " " + date);
+        // create img tag for the Symbol
+        var weathersymbol = $("<img>");
+            //populate symbol with appropriate weather image based on data
+                weathersymbol.attr("src", );
+            // append image into h4
+            title.append(weathersymbol);
+    //create a p tag for temp
+    var temp = $("<p>");
+        //fill text with temp data
+        temp.text();
+    //create a p tag for humidity
+    var humidity = $("<p>")
+        //fill text with humidity data
+        humidity.text();
+    //create a p tag for wind speed
+    var wind = $("<p>");
+        //fill text with wind speed data
+        wind.text();
+    //create a p tag for UV index  
+    var UV = $("<p>");
+        //fill text with UV index data
+        UV.text();
+    //append h4 tag to card
+    newCard.append(title);
+    //append temp tag to card
+    newCard.append(temp);
+    //append humid tag to card
+    newCard.append(humidity);
+    //append wind tag to card
+    newCard.append(wind);
+    //append UV tag to card
+    newCard.append(UV);
+    //append card to new row
+    newRow.append(newCard);
+});
 
     // search the api for the info we need for 5 day 
     $.ajax({
         url: queryurl5Day,
         method: "GET"
-    }).then({
-
+    }).then(function(response){
+        console.log(response);        
     // then 
 
     // create second row for 5 day forecast
-
+    var newRow2 = $("<div>").addClass("row"); 
     //create 5 day forecast h4
+    var fiveday = $("<h4>");
         //h4 text is 5 Day Forecast
+        fiveday.text("5 Day Forecast");
     //append h4 to row 
-
+        newRow2.append(fiveday);
     // create div for day 1
+    var dayone = $("<div>");
         // create day 1 card 
+        var dayonecard= $("<div>").addClass("card bg-primary text-white");
             // populate card with content 
                 // p of that day's date
+                var dateone= $("<p>").text();
                 // img of the weather symbol
+                var symbolone = $("<img>").attr("src",);
                 // p of temp
+                var tempone= $("<p>").text();
                 // p of humidity
+                var humidone= $("<p>").text();
             // append date to card
+            dayonecard.append(dateone);
             // append img to card
+            dayonecard.append(symbolone);
             // append temp to card
+            dayonecard.append(tempone);
             // append humidity to card
+            dayonecard.append(humidone);
         // append card to div
+        dayone.append(dayonecard);
         // append div to row 
+        newRow2.append(dayone);
 
     // create div for day 2
+    var daytwo = $("<div>");
         // create day 2 card 
+        var daytwocard= $("<div>").addClass("card bg-primary text-white");
             // populate card with content 
-                // p of that day's date
-                // img of the weather symbol
-                // p of temp
-                // p of humidity
-            // append date to card
-            // append img to card
-            // append temp to card
-            // append humidity to card
+              // p of that day's date
+              var datetwo= $("<p>").text();
+              // img of the weather symbol
+              var symboltwo = $("<img>").attr("src",);
+              // p of temp
+              var temptwo= $("<p>").text();
+              // p of humidity
+              var humidtwo= $("<p>").text();
+          // append date to card
+          daytwocard.append(datetwo);
+          // append img to card
+          daytwocard.append(symboltwo);
+          // append temp to card
+          daytwocard.append(temptwo);
+          // append humidity to card
+          daytwocard.append(humidtwo);
         // append card to div
+        daytwo.append(daytwocard);
         // append div to row
+        newRow2.append(daytwo);
 
     // create div for day 3
+    var daythree = $("<div>");
         // create day 3 card 
+        var daythreecard= $("<div>").addClass("card bg-primary text-white");
             // populate card with content 
                 // p of that day's date
+                var datethree= $("<p>").text();
                 // img of the weather symbol
+                var symbolthree = $("<img>").attr("src",);
                 // p of temp
+                var tempthree= $("<p>").text();
                 // p of humidity
+                var humidthree= $("<p>").text();
             // append date to card
+            daythreecard.append(datethree);
             // append img to card
+            daythreecard.append(symbolthree);
             // append temp to card
+            daythreecard.append(tempthree);
             // append humidity to card
+            daythreecard.append(humidthree);
         // append card to div
+        daythree.append(daythreecard);
         // append div to row 
+        newRow2.append(daythree);
 
     // create div for day 4
+    var dayfour = $("<div>");
         // create day 4 card 
+        var dayfourcard= $("<div>").addClass("card bg-primary text-white");
             // populate card with content 
                 // p of that day's date
+                var datefour= $("<p>").text();
                 // img of the weather symbol
+                var symbolfour = $("<img>").attr("src",);
                 // p of temp
+                var tempfour= $("<p>").text();
                 // p of humidity
+                var humidfour= $("<p>").text();
             // append date to card
+            dayfourcard.append(datefour);
             // append img to card
+            dayfourcard.append(symbolfour);
             // append temp to card
+            dayfourcard.append(tempfour);
             // append humidity to card
+            dayfourcard.append(humidfour);
         // append card to div
+        dayfour.append(dayfourcard);
         // append div to row 
+        newRow2.append(dayfour);
 
     // create div for day 5
+    var dayfive = $("<div>");
         // create day 5 card 
+        var dayfivecard= $("<div>").addClass("card bg-primary text-white");
             // populate card with content 
                 // p of that day's date
+                var datefive= $("<p>").text();
                 // img of the weather symbol
+                var symbolfive = $("<img>").attr("src",);
                 // p of temp
+                var tempfive= $("<p>").text();
                 // p of humidity
+                var humidfive= $("<p>").text();
             // append date to card
+            dayfivecard.append(datefive);
             // append img to card
+            dayfivecard.append(symbolfive);
             // append temp to card
-            // append humidity to card  
+            dayfivecard.append(tempfive);
+            // append humidity to card
+            dayfivecard.append(humidfive); 
         // append card to div
+        dayfive.append(dayfivecard);
         // append div to row 
+        newRow2.append(dayfive);
 
-
-    
-    // append row 1 to newLocation div 
-    // append row 2 to newLocation div 
-
-    })
+    // append row 1 to resultsArea div 
+    resultsArea.append(newRow);
+    // append row 2 to resultsArea div 
+    resultsArea.append(newRow2);
+    });
 });
